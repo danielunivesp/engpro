6229c36b6b6129d913e4e9edd08dcf423fa62c88
Daniel Cunha <1822981@aluno.univesp.br>
2019-05-09 01:52:32 +0000

    Aula 4

---
+++ src/CasaRet.java
@@ -0,0 +1,13 @@
+/*
+Programa para calcular a área
+de uma casa (e seus cômodos) de
+3 cômodos: uma sala de 10X10m,
+um banheiro e um quarto de 5X7m
+cada.
+*/
+public class CasaRet {
+
+    public static void main(String[] args) {
+ 	    System.out.println("Programa para cálculo \nda área da casa");
+    }
+}
